bef5c93cdd408b84494f0ccdabc72e452053ac88
ziyad meftah <ziyadmeftah@gmail.com>
2020-02-13 17:25:04 +0100

    Added second container to the detail function

--- app.py
+++ app.py
@@ -36,21 +36,36 @@ def item_details(id):
     return(id_req)
 
 @app.route('/detail/<string:doc_id>', methods=['GET'])
+
 def detail(doc_id):
        
-    req = requests.get("http://0.0.0.0/stella/api/v1/recommend_dataset/" + doc_id).json()
-    print(req.values())
+    req = requests.get("http://193.175.238.15:5555/api/recommand_dataset/" + doc_id).json()
     results = req
     l = {} 
-    for key in results['similar_items']:
-        detail = item_details(key)
-        if detail["type"] == "research_data" : 
-            if "publisher" not in detail:
-                l[key] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher": "Unidentified", "Type": detail["type"] }
-            else : 
-                l[key] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher": detail["publisher"], "Type": detail["type"] }
-        
+    if results["container"] == "tf-idf-container" :
+        for key in results['similar_items']:
+            detail = item_details(key)
+            if detail["type"] == "research_data" : 
+                if "publisher" not in detail:
+    #               l[key] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher":detail["publisher"], "Type": detail["type"] }
+                    l[key] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher": "Unidentified", "Type": detail["type"] }
+                else : 
+                    l[key] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher": detail["publisher"], "Type": detail["type"] }
+    else : 
+        for key in range(len(results['similar_items'])):
+            o = (results['similar_items'][key][0]).encode('ascii', 'ignore')
+            detail = item_details(o)
+            if detail["type"] == "research_data" : 
+                if "publisher" not in detail:
+    #               l[key] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher":detail["publisher"], "Type": detail["type"] }
+                    l[o] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher": "Unidentified", "Type": detail["type"] }
+                else : 
+                    l[o] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher": detail["publisher"], "Type": detail["type"] }
+            
+            
+    
     return render_template('detail.html', results=req, id_results = l , query = doc_id)
+
 
 
 if __name__ == '__main__':
