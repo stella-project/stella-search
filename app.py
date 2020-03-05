c8d7d16453aeb0a382b666266801e799976ba611
ziyad meftah <ziyadmeftah@gmail.com>
2020-03-05 12:10:45 +0100

    back to http + fixed format

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-
+import os 
 import requests
 
 app = Flask(__name__)
@@ -35,38 +35,36 @@ def item_details(id):
     id_req = requests.get("http://193.175.238.15:5555/api/items/" + id).json()
     return(id_req)
 
+
 @app.route('/detail/<string:doc_id>', methods=['GET'])
-
 def detail(doc_id):
-       
-    req = requests.get("http://193.175.238.15:5555/api/recommand_dataset/" + doc_id).json()
-    results = req
-    l = {} 
-    if results["container"] == "tf-idf-container" :
-        for key in results['similar_items']:
-            detail = item_details(key)
-            if detail["type"] == "research_data" : 
-                if "publisher" not in detail:
+    results = requests.get(" http://0.0.0.0/stella/api/v1/recommend_dataset/" + doc_id).json()
+    l = {}
+    for i in range(len(results['similar_items'])):
+        key = results['similar_items'][i]["id"]
+        detail = item_details(key)
+        if detail["type"] == "research_data" :
+            if "publisher" not in detail:
+                l[key] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher": "Unidentified", "Type": detail["type"] }
+            else :
+                l[key] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher": detail["publisher"], "Type": detail["type"] }
+'''        else : 
+            for key in range(len(results['similar_items'])):
+                o = (results['similar_items'][key][0]).encode('ascii', 'ignore')
+                detail = item_details(o)
+                if detail["type"] == "research_data" : 
+                    if "publisher" not in detail:
     #               l[key] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher":detail["publisher"], "Type": detail["type"] }
-                    l[key] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher": "Unidentified", "Type": detail["type"] }
-                else : 
-                    l[key] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher": detail["publisher"], "Type": detail["type"] }
-    else : 
-        for key in range(len(results['similar_items'])):
-            o = (results['similar_items'][key][0]).encode('ascii', 'ignore')
-            detail = item_details(o)
-            if detail["type"] == "research_data" : 
-                if "publisher" not in detail:
-    #               l[key] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher":detail["publisher"], "Type": detail["type"] }
-                    l[o] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher": "Unidentified", "Type": detail["type"] }
-                else : 
-                    l[o] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher": detail["publisher"], "Type": detail["type"] }
-            
-            
-    
-    return render_template('detail.html', results=req, id_results = l , query = doc_id)
-
-
+                        l[o] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher": "Unidentified", "Type": detail["type"] }
+                    else : 
+                        l[o] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher": detail["publisher"], "Type": detail["type"] }
+ '''           
+    return render_template('detail.html', results=results, id_results = l , query = doc_id)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+
+    if port == 5000:
+        app.debug = True
+
+    app.run(host='0.0.0.0', port=port)
