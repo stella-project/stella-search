7f036d738595cfc4f65011a7dcd2be8e05b879ec
breuert <timo.breuer@th-koeln.de>
2020-05-18 18:44:16 +0200

    add dataset recommendations

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import os
 import requests
 import json
 import random
+import ast
 
 JL_PATH = './data/index'
 
@@ -28,6 +29,7 @@ def doc_list(id_list):
 def single_doc(id):
     doc = corpus.get(id)
     random_ids = random.choices(list(corpus.keys()), k=4)
+    # req = requests.get("http://0.0.0.0:8080/stella/api/v1/recommendation/publications?item_id=" + id).json()
     recommendations = doc_list(random_ids)
 
     return {'title': doc['title'],
@@ -66,29 +68,47 @@ def index():
 
         results = doc_list(id_list)
 
-    # return render_template('index.html', form=form, results=results)
-    return render_template('index_pubmed.html', form=form, results=results)
+    return render_template('index.html', form=form, results=results)
+    # return render_template('index_pubmed.html', form=form, results=results)
 
 
 def item_details(id):
-    id_req = requests.get("http://193.175.238.15:5555/api/items/" + id).json()
-    return(id_req)
+    # id_req = requests.get("http://193.175.238.15:5555/api/items/" + id).json()
+    # return(id_req)
+    return {"title": "title goes here",
+            "id": id,
+            "date": "date goes here",
+            "publisher": "publisher goes here",
+            "type": "type goes here"}
 
 
 @app.route('/detail/<string:doc_id>', methods=['GET'])
 def detail(doc_id):
-    results = requests.get(" http://0.0.0.0/stella/api/v1/recommend_dataset/" + doc_id).json()
-    l = {}
-    for i in range(len(results['similar_items'])):
-        key = results['similar_items'][i]["id"]
-        detail = item_details(key)
-        if detail["type"] == "research_data" :
-            if "publisher" not in detail:
-                l[key] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher": "Unidentified", "Type": detail["type"] }
-            else :
-                l[key] = {"Title": detail["title"], "Id": detail["id"], "Date": detail["date"],"Publisher": detail["publisher"], "Type": detail["type"] }
+    l = []
+    doc = single_doc(doc_id)
+    try:
+        results = requests.get("http://0.0.0.0:8080/stella/api/v1/recommendation/datasets?item_id=" + doc_id).json()
 
-    return render_template('detail.html', results=results, id_results = l , query = doc_id)
+        for k, v in results.items():
+            id = v.get('docid')
+            detail = item_details(id)
+
+            if "publisher" not in detail:
+                l.append({"Title": detail["title"],
+                          "Id": detail["id"],
+                          "Date": detail["date"],
+                          "Publisher": "Unidentified",
+                          "Type": detail["type"]})
+            else:
+                l.append({"Title": detail["title"],
+                          "Id": detail["id"],
+                          "Date": detail["date"],
+                          "Publisher": detail["publisher"],
+                          "Type": detail["type"]})
+    except Exception as e:
+        raise e
+
+    return render_template('detail.html', result=doc, similar_items=l, query=doc_id)
 
 
 @app.route('/detail_pubmed/<string:doc_id>', methods=['GET'])
