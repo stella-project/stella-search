5d5b427ff07fe4410765c1d9050ff3f0250914e0
Deniz Türkmen <Deniz.Tuerkmen@gesis.org>
2025-02-19 21:06:03 +0100

    Fix: feedback format Add: dataset recommendations, clickable datasets

--- app/stella-search.py
+++ app/stella-search.py
@@ -14,7 +14,16 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 STELLA_APP_API = 'http://stella-app:8000/stella/api/v1/'
-JL_PATH = '../data/index'
+PUB_PATH = '../data/index/publication.jsonl'
+DATA_PATH = '../data/index/dataset.jsonl'
+
+ranking_list = []
+publication_list = []
+dataset_list = []
+ranking_id = None
+pub_rec_id = None
+data_rec_id = None
+rid = None
 
 def index_data(jl_path):
     corpus = {}
@@ -26,60 +35,56 @@ def index_data(jl_path):
     return corpus
 
 
-def doc_list(id_list):
-    logging.info(">>>>>>>>> LEN CORPUS %s", str(len(corpus)))
-    logging.info(">>>>>>>>> ID LIST %s", str(id_list))
-    return [corpus.get(id) for id in id_list]
+def doc_list(id_list, type):
+    if type == 'publication':
+        return [publication_corpus.get(id) for id in id_list]
+    elif type == 'dataset':
+        return [dataset_corpus.get(id) for id in id_list]
 
+def send_feedback(result_type):
+    global ranking_list, publication_list, dataset_list, ranking_id, pub_rec_id, data_rec_id, rid
 
-def single_doc(id):
-    doc = corpus.get(id)
-    logging.info(">>>>>>>>> DOC %s", str(doc))
-    # random_ids = random.choices(list(corpus.keys()), k=4)
-    req = requests.get(
-        STELLA_APP_API + "recommendation/publications?itemid=" + id
-    ).json()
-    logging.info(f">>>>>>>>> REQUEST {req}")
-    recommendations = doc_list([v["docid"] for k, v in req.get("body").items()])
-    logging.info(f">>>>>>>>> Recommendatons for {recommendations}")
-    # send feedback for recommendation of publications
-    click_dict = req.get("body")
-    rec_id = req.get("header").get("rid")
+    if result_type == 'RANK':
+        if rid == ranking_id:
+            return
+        result_list = ranking_list
+        rid = ranking_id
+    elif result_type == 'REC_PUB':
+        if rid == pub_rec_id:
+            return
+        result_list = publication_list
+        rid = pub_rec_id
+    else:
+        if rid == data_rec_id:
+            return
+        result_list = dataset_list
+        rid = data_rec_id
+
+    
+
+    click_dict = {}
+    for i, d in enumerate(result_list):
+        click_dict[str(i)] = d
+    
     if len(click_dict) > 0:
         session_start_date = datetime.datetime.now()
         session_end_date = session_start_date + datetime.timedelta(
             0, random.randint(10, 3000)
         )
-        rand_int = random.randint(1, len(click_dict))
         random_clicks = random.sample(range(1, len(click_dict)), random.randint(1, 3))
-        random.sample(range(1, 10), random.randint(1, 9))
         for key, val in click_dict.items():
             if int(key) in random_clicks:
-                click_dict.update(
-                    {
-                        key: {
-                            "docid": req.get("body").get(key).get("docid"),
-                            "system": req.get("body").get(key).get("type"),
-                            "clicked": True,
-                            "date": session_start_date.strftime("%Y-%m-%d %H:%M:%S"),
-                        }
-                    }
-                )
+                click_dict.update({key: {'docid': result_list[int(key)].get('docid'),
+                                             'type': result_list[int(key)].get('type'),
+                                             'clicked': True,
+                                             'date': session_start_date.strftime("%Y-%m-%d %H:%M:%S")}})
             else:
-                click_dict.update(
-                    {
-                        key: {
-                            "docid": req.get("body").get(key).get("docid"),
-                            "system": req.get("body").get(key).get("type"),
-                            "clicked": False,
-                            "date": None,
-                        }
-                    }
-                )
+                click_dict.update({key: {'docid': result_list[int(key)].get('docid'),
+                                             'type': result_list[int(key)].get('type'),
+                                             'clicked': False,
+                                             }})
 
-        # have to use json.dumps(click_dict) since requests cannot send dict in dict as payload
-        # see also this thread: https://stackoverflow.com/questions/38380086/sending-list-of-dicts-as-value-of-dict-with-requests-post-going-wrong
-        # the stella-server will accept string-formatted json as well as conventional json
+        
         payload = {
             "start": session_start_date.strftime("%Y-%m-%d %H:%M:%S"),
             "end": session_end_date.strftime("%Y-%m-%d %H:%M:%S"),
@@ -87,30 +92,18 @@ def single_doc(id):
             "clicks": json.dumps(click_dict),
             # 'clicks': click_dict
         }
-        req = requests.post(
-            STELLA_APP_API + "recommendation/" + str(rec_id) + "/feedback", data=payload
-        )
 
-
-    abstract = None
-    if doc['abstract'] is not None:
-        if type(doc['abstract']) is list:
-            abstract = doc['abstract'][0]
+        if result_type == 'RANK':
+            feedback_req = requests.post(
+                STELLA_APP_API + "ranking/" + str(rid) + "/feedback", data=payload,
+            )
         else:
-            abstract = doc['abstract']
-    if abstract is not None:
-        abstract = abstract[:1200] + '...'
-    else:
-        abstract = 'nix hier'
+            feedback_req = requests.post(
+                STELLA_APP_API + "recommendation/" + str(rid) + "/feedback", data=payload
+            )
 
-    # return recommendations
-    return {'title': doc['title'],
-            'type': 'publication', #doc['type'],
-            'id': id,
-            'source': 'Unidentified', #doc['publisher'],
-            # 'abstract': (doc['abstract'][0] if type(doc['abstract']) is list else doc['abstract'])[:500] + '...',
-            'abstract': abstract,
-            'similar_items': recommendations[:3]}
+        feedback_req_json = json.loads(feedback_req.text)
+        logging.info(f">>>>>>>>> FEEDBACK RESPONSE ({rid}):  {feedback_req_json}")
 
 
 class SearchForm(FlaskForm):
@@ -126,76 +119,36 @@ bootstrap.init_app(app)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    results = None
+    global ranking_list, ranking_id
+    results = []
     form = SearchForm()
-    if form.validate_on_submit():
-        query = form.query.data
-        form.query.data = query
-        req = requests.get(STELLA_APP_API + "ranking?query=" + query).json()
-        logging.info(">>>>>>>>> REQ %s", req)
 
-        result_list = req.get("body")
-        logging.info(">>>>>>>>> RESULT LIST %s", result_list)
+    try:
+        if form.validate_on_submit():
+            query = form.query.data
+            form.query.data = query
+            req = requests.get(STELLA_APP_API + "ranking?query=" + query).json()
+            #logging.info(">>>>>>>>> REQ %s", req)
 
-        id_list = [doc["docid"] for doc in result_list]
-        logging.info(">>>>>>>>> ID LIST %s", id_list)
+            ranking_list = req.get("body")
+            #logging.info(">>>>>>>>> RESULT LIST %s", ranking_list)
 
-        results = doc_list(id_list)
+            id_list = [doc["docid"] for doc in ranking_list]
+            #logging.info(">>>>>>>>> ID LIST %s", id_list)
 
-        logging.info(
-            ">>>>>>>>> Retrieved %s results for query: %s", len(results), query
-        )
+            results = doc_list(id_list, 'publication')
 
-        # send feedback directly after the ranking is retrieved
-        session_id = req.get("header").get("sid")
-        ranking_id = req.get("header").get("rid")
-
-        click_dict = {}
-        for d in result_list:
-            ind = str(result_list.index(d))
-            click_dict[ind] = d
-
-        if len(click_dict) > 0:
-            session_start_date = datetime.datetime.now()
-            session_end_date = session_start_date + datetime.timedelta(
-                0, random.randint(10, 3000)
+            logging.info(
+                ">>>>>>>>> Retrieved %s results for query: %s", len(results), query
             )
-            rand_int = random.randint(1, len(click_dict))
-            random_clicks = random.sample(
-                range(1, len(click_dict)), random.randint(1, 3)
-            )
-            random.sample(range(1, 10), random.randint(1, 9))
-            for key, val in click_dict.items():
-                if int(key) in random_clicks:
-                    click_dict.update({key: {'docid': result_list[int(key)].get('docid'),
-                                             'system': result_list[int(key)].get('type'),
-                                             'clicked': True,
-                                             'date': session_start_date.strftime("%Y-%m-%d %H:%M:%S")}})
-                else:
-                    click_dict.update({key: {'docid': result_list[int(key)].get('docid'),
-                                             'system': result_list[int(key)].get('type'),
-                                             'clicked': False,
-                                             'date': None}})
 
-            # have to use json.dumps(click_dict) since requests cannot send dict in dict as payload
-            # see also this thread: https://stackoverflow.com/questions/38380086/sending-list-of-dicts-as-value-of-dict-with-requests-post-going-wrong
-            # the stella-server will accept string-formatted json as well as conventional json
-            payload = {
-                "start": session_start_date.strftime("%Y-%m-%d %H:%M:%S"),
-                "end": session_end_date.strftime("%Y-%m-%d %H:%M:%S"),
-                "interleave": True,
-                "clicks": json.dumps(click_dict),
-                # 'clicks': click_dict
-            }
+            # send feedback directly after the ranking is retrieved
+            #session_id = req.get("header").get("sid")
+            ranking_id = req.get("header").get("rid")
 
-            r = requests.post(
-                STELLA_APP_API + "ranking/" + str(ranking_id) + "/feedback",
-                data=payload,
-            )
-            # r_json = json.loads(r.text)
-            # print(r_json, ranking_id)
-
-        logging.info(">>>>>>>>> Results" + str(results))
+            #logging.info(">>>>>>>>> Results: " + str(results))
+    except Exception as e:
+        logging.error(">>>>>>>>> ERROR %s", e)
 
     return render_template("index.html", form=form, results=results)
 
@@ -210,109 +163,118 @@ def item_details(id):
     }
 
 
-@app.route("/detail/<string:docid>", methods=["GET"])
-def detail(docid):
-    l = []
-    doc = single_doc(docid)
 
+def get_publication_recommendations(docid):
+    global publication_list, pub_rec_id
+    try:
+        doc = publication_corpus.get(docid)
+        req = requests.get(
+            STELLA_APP_API + "recommendation/publications?itemid=" + docid
+        ).json()
+        logging.info(f">>>>>>>>> REQUEST {req}")
+        pub_ids = doc_list([v["docid"] for k, v in req.get("body").items()], 'publication')
+
+        publication_list = list(req.get("body").values())
+        pub_rec_id = req.get("header").get("rid")
+
+
+        abstract = None
+        if doc['abstract'] is not None:
+            if type(doc['abstract']) is list:
+                abstract = doc['abstract'][0]
+            else:
+                abstract = doc['abstract']
+        if abstract is not None:
+            abstract = abstract[:1200] + '...'
+        else:
+            abstract = 'nix hier'
+
+        return {'title': doc['title'],
+                'type': 'publication', #doc['type'],
+                'id': docid,
+                'source': 'Unidentified', #doc['publisher'],
+                # 'abstract': (doc['abstract'][0] if type(doc['abstract']) is list else doc['abstract'])[:500] + '...',
+                'abstract': abstract,
+                'similar_items': pub_ids[:3]}
+    except Exception as e:
+        logging.error(">>>>>>>>> ERROR %s", e)
+
+        return {'title': '',
+                'type': '', #doc['type'],
+                'id': docid,
+                'source': 'Unidentified', #doc['publisher'],
+                # 'abstract': (doc['abstract'][0] if type(doc['abstract']) is list else doc['abstract'])[:500] + '...',
+                'abstract': '',
+                'similar_items': []}
+
+
+def get_dataset_recommendations(docid):
+    global dataset_list, data_rec_id
+    rec_list = []
     try:
 
-        results = requests.get(
+        req = requests.get(
             STELLA_APP_API + "recommendation/datasets?itemid=" + docid
         ).json()
 
-        # send feedback for recommendation of publications
-        click_dict = results.get("body")
-        rec_id = results.get("header").get("rid")
-        if len(click_dict) > 0:
-            session_start_date = datetime.datetime.now()
-            session_end_date = session_start_date + datetime.timedelta(
-                0, random.randint(10, 3000)
-            )
-            rand_int = random.randint(1, len(click_dict))
-            random_clicks = random.sample(
-                range(1, len(click_dict)), random.randint(1, 3)
-            )
-            random.sample(range(1, 10), random.randint(1, 9))
-            for key, val in click_dict.items():
-                if int(key) in random_clicks:
-                    click_dict.update(
-                        {
-                            key: {
-                                "docid": results.get("body").get(key).get("docid"),
-                                "system": results.get("body").get(key).get("type"),
-                                "clicked": True,
-                                "date": session_start_date.strftime(
-                                    "%Y-%m-%d %H:%M:%S"
-                                ),
-                            }
-                        }
-                    )
-                else:
-                    click_dict.update(
-                        {
-                            key: {
-                                "docid": results.get("body").get(key).get("docid"),
-                                "system": results.get("body").get(key).get("type"),
-                                "clicked": False,
-                                "date": None,
-                            }
-                        }
-                    )
 
-            # have to use json.dumps(click_dict) since requests cannot send dict in dict as payload
-            # see also this thread: https://stackoverflow.com/questions/38380086/sending-list-of-dicts-as-value-of-dict-with-requests-post-going-wrong
-            # the stella-server will accept string-formatted json as well as conventional json
-            payload = {
-                "start": session_start_date.strftime("%Y-%m-%d %H:%M:%S"),
-                "end": session_end_date.strftime("%Y-%m-%d %H:%M:%S"),
-                "interleave": True,
-                "clicks": json.dumps(click_dict),
-                # 'clicks': click_dict
-            }
-            req = requests.post(
-                STELLA_APP_API + "recommendation/" + str(rec_id) + "/feedback",
-                data=payload,
-            )
+        data_ids = doc_list([v["docid"] for k, v in req.get("body").items()], 'dataset')
+        logging.info(f'>>>>>>>>>>>> DATASETS-2: {data_ids}')
+        for i, d in enumerate(data_ids):
+            if type(d['title']) == list:
+                data_ids[i]['title'] = d['title'][0]
+            if type(d['doi']) == list:
+                data_ids[i]['doi'] = d['doi'][0]
 
-        for k, v in results.get("body").items():
-            id = v.get("doc_id")
+        data_rec_id = req.get("header").get("rid")
+
+        rec_list = data_ids
+        
+        '''
+        for k, v in req.get("body").items():
+            id = v.get("docid")
             detail = item_details(id)
 
-            if "publisher" not in detail:
-                l.append(
+            rec_list.append(
                     {
                         "Title": detail["title"],
                         "Id": detail["id"],
                         "Date": detail["date"],
-                        "Publisher": "Unidentified",
+                        "Publisher": detail.get('publisher', 'Unidentified'),
                         "Type": detail["type"],
                     }
-                )
-            else:
-                l.append(
-                    {
-                        "Title": detail["title"],
-                        "Id": detail["id"],
-                        "Date": detail["date"],
-                        "Publisher": detail["publisher"],
-                        "Type": detail["type"],
-                    }
-                )
+            )
+        '''
     except Exception as e:
         logging.error(">>>>>>>>> ERROR %s", e)
         # raise e
         pass
 
-    return render_template("detail.html", result=doc, similar_items=l[:3], query=docid)
+    dataset_list = rec_list
+    return rec_list
+
+    
+
+
+@app.route("/detail/<string:docid>/<string:result_type>", methods=["GET"])
+def detail(docid, result_type):
+    send_feedback(result_type)
+
+    publications_rec = get_publication_recommendations(docid)
+    datasets_rec = get_dataset_recommendations(docid)
+
+    return render_template("detail.html", publications=publications_rec, datasets=datasets_rec[:3], query=docid)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    for file in os.listdir(JL_PATH):
-        logging.info(">>>>>>>>> FILE %s", file)
-        if file.endswith(".jsonl"):
-            corpus = index_data(os.path.join(JL_PATH, file))
+    publication_corpus = {}
+    dataset_corpus = {}
+    if os.path.exists(PUB_PATH):
+        publication_corpus = index_data(PUB_PATH)
+
+    if os.path.exists(DATA_PATH):
+        dataset_corpus = index_data(DATA_PATH)
     if port == 5000:
         app.debug = True
 
